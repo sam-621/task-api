13f2d1f624a9e48204f7b289e6073c5ee9e8d464
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-11-06 15:03:19 -0700

    add setupExpressMiddleware

--- src/modules/app.ts
+++ src/modules/app.ts
@@ -1,15 +1,26 @@
 import express, { Application } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 export class App {
   private app: Application;
 
   constructor() {
     this.app = express();
+    this.setupExpressMiddleware();
   }
 
   public start(port: number) {
     this.app.listen(port, () => {
       console.log('listening');
     });
+  }
+
+  private setupExpressMiddleware() {
+    this.app.use(cors());
+    this.app.use(helmet());
+    this.app.use(morgan('dev'));
+    this.app.use(express.json());
   }
 }
